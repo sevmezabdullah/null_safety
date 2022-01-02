1507859956814d3fe98bf04079acafd383677646
sevmezabdullah <abdullahsevmez@gmail.com>
2022-01-02 21:16:41 +0300

    init

---
+++ bin/null_safety.dart
@@ -0,0 +1,13 @@
+//Dart dilinde non-nullable ve nullable değişkenler vardır. Bütün değişkenler default olarak non-nullable olarak gelir. Ancak ? operatörü kullanılarak //değişkeni nullable yapabiliriz. Bu sayede oluşturduğumuz değişkene null değerininde gelebileceğini söylemiş oluruz.
+
+// nullValue değişkenine null değeri atamak istediğimizde hata verdiğini göreceksiniz.
+//String nullValue = null;
+
+// cityName değişkenine değer atanmadığı içinde hata verdiğini göreceksiniz.
+//String cityName;
+
+//name değişkenini nullable olarak atadığımız için hiç bir değer gelmezse null olarak değer alacaktır.
+//String? name;
+void main(List<String> arguments) {
+ // print(name);
+}
